3942cb2cdb2b77628aba1cc4465b55cc6b1fc64c
666sora <pavelbys03@gmail.com>
2022-05-05 22:43:27 +0700

    3-task done

---
+++ 3-task.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream statement;
+    statement.open("statement.txt");
+    int sum = 0;
+    int max = 0;
+    std::string maxName;
+    while (!statement.eof()) {
+        std::string firstName, secondName, data;
+        int count;
+        statement >> firstName >> secondName >> count >> data;
+        sum += count;
+        if (count > max) {
+            max = count;
+            maxName = firstName;
+            maxName += ' ' + secondName;
+        }
+    }
+    statement.close();
+    std::cout << "Name and surname of person who contributed max money is " << maxName << std::endl;
+    std::cout << "Total sum is " << sum;
+}
+
+/*
+Реализуйте программу чтения информации из ведомости по учёту выплат. Сама ведомость представляет собой следующую таблицу: 
+имя и фамилия человека; 
+количество денег, которые были выплачены лицу; 
+дата выплаты в формате ДД.ММ.ГГГГ.
+
+На основе данных из ведомости следует подсчитать общее количество выплаченных средств и определить человека с максимальной суммой выплат.
+*/
